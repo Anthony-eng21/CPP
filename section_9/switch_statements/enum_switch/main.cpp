381749835cfc001f6ecd24c6699f97488755fd3c
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-07-07 23:12:22 -0600

    El Switch C++
    nice, added an enumerative type for our integral control type lol.

---
+++ section_9/switch_statements/enum_switch/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //enumeration type with constants 
+    //Direction is the referenc name for this new type
+    //the compiler knows of these values so if you fail to add all of them in
+    // this syntax it'll throw a notice unless you have a default value of course
+    enum Direction {
+        left, right, up, down
+    };
+
+    //heading is of type direction
+    Direction heading {left};
+
+    //this variable heading in the switch case could be any of the above constant cases. so they need to be included for best practice
+    //or you can make an individual type for any of the constants
+    switch (heading)
+    {
+    case left:
+        cout << "going left" << endl;
+        break;
+    
+    case right:
+         cout << "Going right" << endl;
+         break;
+    
+    case up:
+         cout << "Going up" << endl;
+         break;
+    
+    case down:
+         cout << "Going down" << endl;
+         break;
+    
+    default:
+        cout << "OK" << endl;
+        break;
+    }
+
+    cout << endl;
+    return 0;
+}
--- section_9/switch_statements/main.cpp
+++ section_9/switch_statements/main.cpp
@@ -5,6 +5,55 @@ using namespace std;
 
 int main()
 {
+    char letter_grade {};
+
+    cout << "Enter the letter grade you expect on the exam:";
+    cin >> letter_grade;
+
+    switch (letter_grade)
+    {
+    case 'a':
+    case 'A':
+        cout << "You need a 90 or above, study hard!" << endl;
+        break;
+
+    case 'b':
+    case 'B':
+        cout << "You need a 80-89 for a B, go study" << endl;
+        break;
+
+    case 'c':
+    case 'C':
+        cout << "You need a 70-79 for a C, average grade some to no studying" << endl;
+        break;
+
+    case 'd':
+    case 'D':
+        cout << "You need a 60-69 for a D, strive for a better grade" << endl;
+        break;
+
+    //F has additional logic i.e the extra code block where we confirm with the student that they really want to fail
+    case 'f':
+    case 'F':
+    //make a code block here because we declare a new variable 
+    {
+        char confirm{0};
+        cout << "Are you sure (Y/N)?";
+        cin >> confirm;
+
+        if(confirm == 'y' || confirm == 'Y') 
+            cout << "Okay I guess you don't want to study" << endl;
+        else if (confirm == 'n' || confirm == 'N')
+            cout <<"Good, go study" << endl;
+        else 
+            cout << "Illegal choice (just flunk the class then)";
+
+        break;
+    }
+
+    default: 
+        cout << "Sorry, that's not a valid grade.";
+    }
 
     cout << endl;
     return 0;
