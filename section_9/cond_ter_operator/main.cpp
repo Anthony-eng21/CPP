fa70fd1089e49baf28956102c7191783e96a1305
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-07-10 11:28:28 -0600

    cond op c++
    more cond operator

--- section_9/cond_ter_operator/main.cpp
+++ section_9/cond_ter_operator/main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 
-//conditional operator
+// conditional operator
+//Usually found it in loops to loop over some specific data based off some condition 
 
 using namespace std;
 
 int main()
 {
-    int num {};
+    int num{};
 
     cout << "Enter an integer";
     cin >> num;
 
-    if (num % 2 == 0) 
+    if (num % 2 == 0)
         cout << num << " is even" << endl;
-    else 
+    else
         cout << num << " is odd" << endl;
-    //with cond operator
-                                //condition  / ternary expression 
-    cout << num << " is " <<  ((num % 2 == 0) ? "Even" : "Odd") << endl;
+    // with cond operator
+    //    /   /    /     /    /  //condition  / ternary expression
+    cout << num << " is " << ((num % 2 == 0) ? "Even" : "Odd") << endl; // ONE LINER 🤯
+
+    int num1{}, num2{};
+
+    cout << "Enter two integers separated by a space: ";
+    cin >> num1 >> num2; 
+
+    if (num1 != num2)
+    {
+        cout << "Largest: " << ((num1 > num2) ? num1 : num2) << endl;
+        cout << "Smallest: " << ((num1 < num2) ? num1 : num2) << endl;
+    }
+    else
+    {
+        cout << "The numbers are the same" << endl;
+    }
 
     cout << endl;
     return 0;
