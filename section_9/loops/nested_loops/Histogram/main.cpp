fc943eef2135fe7172bee7cbb2f80d9ef7b2cf71
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-07-14 18:25:23 -0700

    Histogram _ nested loops
    really cool way to make a histogram and good way of showing nested loops

--- section_9/loops/nested_loops/Histogram/main.cpp
+++ section_9/loops/nested_loops/Histogram/main.cpp
@@ -1,21 +1,40 @@
+// Section 9
+// Nested Loops - Histogram
+
 #include <iostream>
 #include <vector>
-// INNER LOOP, LOOPS FASTER THAN THE OUTER LOOP
+
 using namespace std;
-int main()
-{
+
+int main() {
+    
     int num_items{};
-
-    cout << "How may data items do you have";
-    cin >> num_items;
-
-    vector<int> data{};
-
-    for (int i{1}; i <= num_items; ++i)
-    {
-        
+    
+    cout << "How many data items do you have? ";
+    cin >> num_items; //user initially writes here.
+    
+    vector<int> data {}; //data vector we store and read data_items
+    
+    for (int i{1}; i<= num_items; ++i) {
+        int data_item{};
+        cout << "Enter data item " << i << ": ";
+        cin >> data_item; //user writes / creates their data items
+        data.push_back(data_item); //stores the data item in the vector after they write their data item
+    }
+    
+    cout << "\nDisplaying Histogram" << endl;
+    for (auto val: data) { //read from data vector where the user data_items are
+        for (int i{1}; i<=val; ++i) {
+            //additonal nifty control statements
+            if (i % 5 == 0) // every i that is divisble by 5 print *
+                cout << " * ";
+            else
+                cout << "#"; // else any other divisible print - dashes
+        }    
+        cout << endl;
     }
 
-    std::cout << std::endl;
+    cout <<  endl;
     return 0;
 }
+
