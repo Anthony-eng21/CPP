8cdc92b8c1186f0d03458dd2a20e7fd16d20f538
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-07-16 02:28:06 -0700

    End of Section Challenge

--- section_9/challenge/main.cpp
+++ section_9/challenge/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -7,11 +8,13 @@ int main()
 {
     vector<int> numbers{}; // users number list
     char selection{};      // user selection p, a, m, s, q, l operations
-
+    string username{};
     do
     {
-    // Display the menu unconditionally
+        // Display the menu unconditionally
         // a do while works great here
+        cout << "Enter your username: ";
+        cin >> username;
         cout << "\nP - Print numbers" << endl;
         cout << "A - Add a number" << endl;
         cout << "M - Display a mean of the numbers" << endl;
@@ -22,11 +25,11 @@ int main()
         cout << "\nEnter your choice: ";
         cin >> selection;
 
-    // Selection logic starts here
+        // Selection logic starts here
         // using character literals for user input not strings
         // using if else if ladder statement for our program flow
 
-    // print nums from our vector<int>numbers {} list
+        // print nums from our vector<int>numbers {} list
         if (selection == 'p' || selection == 'P')
         {
             if (numbers.size() == 0)                // nothing in the list
@@ -39,7 +42,7 @@ int main()
                 cout << "]" << endl;
             }
         }
-    // Add number into our number vec list
+        // Add number into our number vec list
         else if (selection == 'a' || selection == 'A')
         {
             int num_to_add{}; // var we read into that goes into the nuumbers vector
@@ -48,22 +51,60 @@ int main()
             numbers.push_back(num_to_add);          // write into the vector here with pushback
             cout << num_to_add << " added" << endl; // display some info when the num_to_add data was 'saved'
         }
-    // Calc Mean of nums in our list
+        // Calc Mean of nums in our list
         else if (selection == 'm' || selection == 'M')
         {
             if (numbers.size() == 0)
                 cout << "Unable to calc mean - no data" << endl;
 
-            //Stuff in our vector logic here so we need to 
-            //calculate the mea nof our items in our numbers vec
+            // Stuff in our vector logic here so we need to
+            // calculate the mea nof our items in our numbers vec
             else
             {
-                int total{};
-                for(auto num: numbers)
-                    total += num; //get sum of the num(s) here in our numbers vec
-                //static cast here is really nice WE DONT WANT INT DIVISION
+                int total{}; // stores the value of all the nums added together
+                for (auto num : numbers)
+                    total += num; // get sum of the num(s) here in our numbers vec and add them together
+                // static cast here is really nice WE DONT WANT INT DIVISION
                 cout << "The mean is: " << static_cast<double>(total) / numbers.size() << endl;
             }
+        }
+        // Display the smallest int in the list
+        else if (selection == 's' || selection == 'S')
+        {
+            if (numbers.size() == 0)
+                cout << "Unable to determine the list is empty";
+            else
+            {
+                // assume the first element is the one with the lowest int value in the list
+                int smallest = numbers.at(0);
+                for (auto num : numbers)
+                    if (num < smallest) // current num in our loop is smaller than the "smallest"(swap)
+                        smallest = num;
+                cout << "The smallest number in the list is: " << smallest << endl;
+            }
+        }
+        // Display largest int in the list
+        else if (selection == 'l' || selection == 'L')
+        {
+            if (numbers.size() == 0)
+                cout << "Unable to determine the list is empty";
+            else
+            {
+                int largest = numbers.at(0); // similiar logic assume its the first element in the list
+                for (auto num : numbers)
+                    if (num > largest)
+                        largest = num;
+                cout << "The largest number in the list is: " << largest << endl;
+            }
+        }
+        else if (selection == 'q' || selection == 'Q')
+        {
+            cout << "Goodbye " << username << endl;
+        }
+        //Illegal selection 
+        else 
+        {
+            cout << "Unknown selection, please try again" << endl;
         }
 
     } while (selection != 'q' && selection != 'Q');
